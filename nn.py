de5a9042a7349dfcf2d5f4d49a2ac7c2e97847f6
rpcook <rpcook.htc@gmail.com>
2020-04-16 22:45:27 +0100

    support for neuron bias added

--- nn.py
+++ nn.py
@@ -18,19 +18,29 @@ def dsigmoid(x):
 class network:
     def __init__(self):
         self.__neuronsPerLayer = []
-        self.__neuronActivations = []
+        self.__neuronActivation = []
+        self.__neuronBias = []
     
     def setStructure(self, structure):
         self.__neuronsPerLayer = structure
         self.__neuronActivation = []
         for layerSize in structure:
-            self.__neuronActivations.append(np.zeros(layerSize))
+            self.__neuronActivation.append(np.zeros(layerSize))
+        self.__neuronBias = []
+        for layerSize in structure:
+            self.__neuronBias.append(np.zeros(layerSize))
         
     def getStructure(self):
         return self.__neuronsPerLayer
     
     def setNeuronActivation(self, layer, neuron, activation):
-        self.__neuronActivations[layer][neuron] = activation
+        self.__neuronActivation[layer][neuron] = activation
     
     def getNeuronActivation(self, layer, neuron):
-        return self.__neuronActivations[layer][neuron]
+        return self.__neuronActivation[layer][neuron]
+    
+    def setNeuronBias(self, layer, neuron, bias):
+        self.__neuronBias[layer][neuron] = bias
+    
+    def getNeuronBias(self, layer, neuron):
+        return self.__neuronBias[layer][neuron]
