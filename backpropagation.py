1eb58ace10c6600b6cbb625c7726934f64fd967d
rpcook <rpcook.htc@gmail.com>
2020-04-21 18:35:31 +0100

    added initialiser for neural network

--- backpropagation.py
+++ backpropagation.py
@@ -6,15 +6,18 @@ import nn
 class trainer:
     def __init__(self):
         self.network = nn.network()
-        pass
     
-    def setNetwork(self, network):
-        self.network = network
+    def initialiseNetwork(self, structure, seed):
+        self.network.setStructure(structure)
+        np.random.seed(seed)
+        for i in range(len(structure)-1):
+            self.network.setConnectionWeights(i+1, np.random.random((structure[i+1],structure[i]))*2-1)
+            self.network.setNeuronBias(i+1, range(structure[i+1]), np.random.random(structure[i+1])*2-1)
     
     def getNetwork(self):
         return self.network
     
-    def cost(self, image, result):
-        self.network.setNeuronActivation(0, range(self.network.getStructure()[0]), image)
+    def cost(self, inputLayer, desiredOutput):
+        self.network.setNeuronActivation(0, range(self.network.getStructure()[0]), inputLayer)
         self.network.evaluate()
-        return np.sum((self.network.getNeuronActivation(self.network.getStructure()[-1])-result)**2)
+        return np.sum((self.network.getNeuronActivation(self.network.getStructure()[-1])-desiredOutput)**2)
