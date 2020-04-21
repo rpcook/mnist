2b2bb816f0f821ff828699fd7328c65e88d10ae9
rpcook <rpcook.htc@gmail.com>
2020-04-21 22:06:10 +0100

    network initialiser accepts either a seed or no seed (seed from system clock)

--- backpropagation.py
+++ backpropagation.py
@@ -2,13 +2,16 @@
 
 import numpy as np
 import nn
+from time import time
 
 class trainer:
     def __init__(self):
         self.network = nn.network()
     
-    def initialiseNetwork(self, structure, seed):
+    def initialiseNetwork(self, structure, *seed):
         self.network.setStructure(structure)
+        if len(seed)==0:
+            seed=int((time()-int(time()))*(10**6))
         np.random.seed(seed)
         for i in range(len(structure)-1):
             self.network.setConnectionWeights(i+1, np.random.random((structure[i+1],structure[i]))*2-1)
