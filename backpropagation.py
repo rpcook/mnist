1352cf71d1447e1b903351a739e814e507503e40
rpcook <rpcook.htc@gmail.com>
2020-04-26 01:15:41 +0100

    updated random seed handling added support to load MNIST database

--- backpropagation.py
+++ backpropagation.py
@@ -2,17 +2,19 @@
 
 import numpy as np
 import neuralnetwork as nn
-from time import time
+import mnist
 
 class trainer:
     def __init__(self):
         self.network = nn.network()
+        self.__mnistLoaded = False
     
     def initialiseNetwork(self, structure, *seed):
         self.network.setStructure(structure)
-        if len(seed)==0:
-            seed=int((time()-int(time()))*(10**6))
-        np.random.seed(seed)
+        if len(seed[0])==0:
+            np.random.seed()
+        else:
+            np.random.seed(seed[0])
         for i in range(len(structure)-1):
             self.network.setConnectionWeights(i+1, np.random.random((structure[i+1],structure[i]))*2-1)
             self.network.setNeuronBias(i+1, range(structure[i+1]), np.random.random(structure[i+1])*2-1)
@@ -22,6 +24,13 @@ class trainer:
     
     def getNetwork(self):
         return self.network
+    
+    def checkMNISTload(self):
+        return self.__mnistLoaded
+    
+    def loadMNIST(self):
+        self.mnistData = mnist.database()
+        self.__mnistLoaded = True
     
     def cost(self, inputLayer, desiredOutput):
         self.network.setNeuronActivation(0, range(self.network.getStructure()[0]), inputLayer)
